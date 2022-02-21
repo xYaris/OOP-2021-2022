91219b9ca7fd312f129d7c6e6bbd77541f5bc10e
Yaris <C20394791@mytudublin.ie>
2022-02-21 11:45:36 +0000

    Yaris first

---
+++ java/src/ie/tudublin/Arrays.java
@@ -0,0 +1,26 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Arrays extends PApplet {
+
+    float[] rainfall = { 45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58 };
+
+    public void settings()
+    {
+        size(500, 500);
+
+    }
+
+    public void setup()
+    {
+
+    }
+
+    public void draw()
+    {
+
+    }
+    
+
+}
